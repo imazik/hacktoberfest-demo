1a0da2456a99ce87c3fdcbd63cac9984d6b3fc11
Shovan Maity <shovan.cse91@gmail.com>
2019-10-18 21:05:58 +0530

    initial commit
    Signed-off-by: Shovan Maity <shovan.cse91@gmail.com>

---
+++ account-service-go/db.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
